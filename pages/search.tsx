9161b96f600684acbe21913e95828d8da9c77912
Viet Dinh <54ckb0y789@gmail.com>
2022-10-18 17:50:24 -0400

    Stylize search screen

--- pages/search.tsx
+++ pages/search.tsx
@@ -9,6 +9,27 @@ interface SearchProps {
   initialQuery: string
 }
 
+const now = new Date()
+const commonCurrencies = {
+  USD: '🇺🇸',
+  EUR: '🇪🇺',
+  GBX: '🇬🇧',
+  GBP: '🇬🇧',
+  CNY: '🇨🇳',
+  BRL: '🇧🇷',
+  INR: '🇮🇳'
+} as Record<string, string | undefined>
+
+function parseTime(hour: string, tz: string) {
+  if (!tz.startsWith('UTC')) return
+  const timezone = +tz.substring(3)
+
+  const [hour_, minute] = hour.split(':') 
+  const date = new Date()
+  date.setUTCHours(+hour_ - timezone, +minute, 0, 0)
+  return date
+}
+
 const Search: NextPage<SearchProps> = (props) => {
   const searchRef = useRef<HTMLInputElement>()
   const router = useRouter()
@@ -31,18 +52,42 @@ const Search: NextPage<SearchProps> = (props) => {
             placeholder="Search for signals, cryptocurrencies" />
         </label>
       </form>
-      {props.results.map(result => {
-        const symbol = result[BestMatchesKeys.symbol]
-        return (
-          <Link
-            href={`/details/${encodeURIComponent(symbol)}`}
-            key={symbol}>
-            <div className="card cursor-pointer">
-              <pre>{JSON.stringify(result, undefined, 2)}</pre>
-            </div>
-          </Link>
-        )
-      })}
+      <div className="grid lg:grid-cols-2 gap-4">
+        {props.results.map(result => {
+          const symbol = result[BestMatchesKeys.symbol]
+          const tz = result[BestMatchesKeys.timezone]
+          const openTime = parseTime(result[BestMatchesKeys.marketOpen], tz)
+          const closeTime = parseTime(result[BestMatchesKeys.marketClose], tz)
+          const closed = openTime && closeTime && (now > closeTime || now < openTime)
+          const flag = commonCurrencies[result[BestMatchesKeys.currency]] || ''
+          return (
+            <Link
+              href={`/details/${encodeURIComponent(symbol)}`}
+              key={symbol}>
+              <div className="card m-0 cursor-pointer">
+                <h1 title={closed ? 'Closed' : 'Open'} className="text-2xl">
+                  {openTime && closeTime && (
+                    <span className={`pr-1 ${closed ? 'text-red-500' : 'text-green-500'}`}>•</span>
+                  )}
+                  {result[BestMatchesKeys.symbol]}
+                </h1>
+                <p className="text-sm text-slate-500">{result[BestMatchesKeys.name]}{" • "}{result[BestMatchesKeys.region]}</p>
+                <div className="flex flex-row space-x-1 mt-2">
+                  <div
+                    title="Type"
+                    className="badge bg-blue-500 text-white">{result[BestMatchesKeys.type]}</div>
+                  <div
+                    title="Currency"
+                    className="badge bg-orange-500 text-white">
+                    {flag && <span className="pr-1">{flag}</span>}
+                    {result[BestMatchesKeys.currency]}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
       {!props.results.length && <span className="text-3xl text-gray-400 font-bold">No results found.</span>}
     </div>
   )
