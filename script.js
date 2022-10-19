4ef8cef1530d388912020ef5a908ed44c21c4e51
Aditya Thakur <71738434+athakr@users.noreply.github.com>
2022-10-19 18:24:43 -0400

    Landing page
    Basic structure of the landing page with functionality to toggle to dark mode

---
+++ script.js
@@ -0,0 +1,9 @@
+//dark mode function
+function Dark(){
+    var e = document.body;
+    e.classList.toggle("darkmode");
+}
+
+//scroll height function
+    var f = document.getElementById("next-step1");
+    f.scrollTop = f.scrollHeight;
