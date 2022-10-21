2f24559de0fb2cd36bc4eeab64b6c033dcc27220
Steve Pham <steve@Steves-MacBook-Air.local>
2022-10-21 19:14:46 -0400

    Updated info card, glossary added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
 }
 
-module.exports = process.env.ANALYZE
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
+function withAnalyzer(config) {
+  return process.env.ANALYZE
   ? require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === '1'
-  })(nextConfig)
-  : nextConfig
+  })(config)
+  : config
+}
+
+
+module.exports = withAnalyzer(withMDX(nextConfig))
--- pages/details/[symbol].tsx
+++ pages/details/[symbol].tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps, NextPage } from "next";
+import { InformationCircleIcon, ChevronDoubleDownIcon, ChevronDoubleUpIcon } from '@heroicons/react/24/solid';
 import { Line } from "react-chartjs-2";
 import { DatapointKeys, Datapoints, timeseries } from "../../utils/api";
 import {
@@ -12,6 +13,8 @@ import {
 import { useRouter } from "next/router";
 import { uri } from "../../utils/common";
 import HomeLink from "../../components/homelink";
+import { getDefaultSettings } from "http2";
+import Link from "next/link";
 
 ChartJS.register(
   LineElement,
@@ -49,13 +52,35 @@ function toDataset(data: Datapoints, key = DatapointKeys.close) {
   };
 }
 
+function getLatestData(data: Datapoints, key = DatapointKeys.close){
+  const dp: string[] = [];
+  const labels: string[] = [];
+  return Object.entries(data).shift()!
+}
+
+function comparePreviousDay(data: Datapoints, key = DatapointKeys.close){
+  const [
+    [todayDate, todayData],
+    [yesterdayDate, yesterdayData]
+  ] = Object.entries(data)
+
+  const yesterdayHigh = +yesterdayData[key]
+  const todayHigh = +todayData[key]
+  if(yesterdayHigh <= todayHigh){
+    return <ChevronDoubleDownIcon className="h-4 w-4 inline text-red-500 fill-current"></ChevronDoubleDownIcon>
+    
+  }
+  return <ChevronDoubleUpIcon className="h-4 w-4 inline text-green-500 fill-current"></ChevronDoubleUpIcon>
+}
+
 const Details: NextPage<DetailsProps> = (props) => {
   const router = useRouter()
+  const current = new Date()
   return (
     <div className="container mx-auto">
       <HomeLink className="text-3xl mt-4" />
       <div className="card">
-        <h1 className="text-3xl">{props.symbol}</h1>
+        <h1 className="text-3xl">{props.symbol} {`(${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()})`}</h1>
           <select
             name="scale"
             id="scale"
@@ -69,19 +94,63 @@ const Details: NextPage<DetailsProps> = (props) => {
             <option value="weekly">Weekly</option>
             <option value="monthly">Monthly</option>
           </select>
-        <Line
-          data={toDataset(props.data)}
-          options={{
-            responsive: true,
-            scales: {
-              x: { type: "category", reverse: true },
-              y: { type: "linear" },
-            },
-            plugins: {
-              tooltip: {},
-            },
-          }}
-        />
+
+        <div className="grid grid-cols-2">
+
+          <div className="col-span-1 w-30">
+            {(() => {
+              const [date, data] = getLatestData(props.data)
+              return <ul className="list-none text-2xl">
+                <li>
+                  <div className="inline-block">
+                  <Link href="/glossary"><InformationCircleIcon title="Opening price that is first traded within a day.&#013;Click for more info..." className="cursor-pointer h-4 w-4 inline"></InformationCircleIcon></Link>
+                    &nbsp;Open:
+                  </div> {data[DatapointKeys.open]}&nbsp;{comparePreviousDay(props.data, DatapointKeys.open)}
+                </li>
+                <li>
+                  <div className="inline-block">
+                  <Link href="/glossary"><InformationCircleIcon title="Security's intraday highest trading price.&#013;Click for more info..." className="cursor-pointer h-4 w-4 inline"></InformationCircleIcon></Link>
+                    &nbsp;High:
+                  </div> {data[DatapointKeys.high]}&nbsp;{comparePreviousDay(props.data, DatapointKeys.high)}
+                </li>
+                <li>
+                  <div className="inline-block">
+                  <Link href="/glossary"><InformationCircleIcon title="Security's intraday lowest trading price.&#013;Click for more info..." className="cursor-pointer h-4 w-4 inline"></InformationCircleIcon></Link>
+                    &nbsp;Low:
+                  </div> {data[DatapointKeys.low]}&nbsp;{comparePreviousDay(props.data, DatapointKeys.low)}
+                </li>
+                <li>
+                  <div className="inline-block">
+                  <Link href="/glossary"><InformationCircleIcon title="Total number of buyers and sellers exchanging shares within a day. &#010;Click for more info..." className="cursor-pointer h-4 w-4 inline"></InformationCircleIcon></Link>
+                    &nbsp;Volume:
+                  </div> {data[DatapointKeys.volume]}&nbsp;{comparePreviousDay(props.data, DatapointKeys.volume)}
+                </li>
+                <li>
+                  <div className="inline-block">
+                  <Link href="/glossary"><InformationCircleIcon title="Last price that is traded within a day. Click for more info..." className="cursor-pointer h-4 w-4 inline"></InformationCircleIcon></Link>
+                    &nbsp;Close:
+                  </div> {data[DatapointKeys.close]}&nbsp;{comparePreviousDay(props.data, DatapointKeys.close)}
+                </li>
+              </ul>
+            })()}
+          </div>
+
+          <div className="row-span-2">
+            <Line
+              data={toDataset(props.data)}
+              options={{
+                responsive: true,
+                scales: {
+                  x: { type: "category", reverse: true },
+                  y: { type: "linear" },
+                },
+                plugins: {
+                  tooltip: {},
+                },
+              }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
